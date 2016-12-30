385a92f0083b450eea218bd813729364e5de5ecb
Clive Chan <cc@clive.io>
2016-12-30 18:00:53 -0500

    move example from clive-io/statbot

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv-safe').load();
+let ostb = require('os-toolbox');
+
+let statbot = require('statbot')({
+  verify_token: process.env.FB_VERIFY_TOKEN,
+  page_token: process.env.FB_PAGE_TOKEN,
+  app_secret: process.env.FB_APP_SECRET,
+  page_scoped_user_id: process.env.FB_USER_ID,
+  port: process.env.PORT
+});
+
+pm2.connect(() => {
+  pm2.launchBus((err, bus) => {
+    bus.on('log:out', packet => {
+      statbot.say('[' + packet.process.name + '] ' + packet.data);
+    });
+    bus.on('log:err', packet => {
+      statbot.say('[' + packet.process.name + '][err] ' + packet.data);
+    });
+  });
+});
+
+statbot.hears(["status"], (text, reply) => {
+  reply("Uptime: " + ostb.uptime() + "s");
+  ostb.cpuLoad().then(function(cpuusage){
+    reply("CPU: " + cpuusage + "%");
+  });
+  ostb.memoryUsage().then(function(memusage){
+    reply("Memory: " + cpuusage + "%");
+  });
+});
