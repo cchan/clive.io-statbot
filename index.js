68293c7cac3091f21770e7a53b435210f938f2ec
Clive Chan <cc@clive.io>
2017-04-08 18:27:12 -0400

    add ssh watch and newline fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv-safe').load();
 let ostb = require('os-toolbox');
 let pm2 = require('pm2');
+let Tail = require('tail').Tail;
 
 let statbot = require('server-statbot')({
   verify_token: process.env.FB_VERIFY_TOKEN,
@@ -9,14 +10,19 @@ let statbot = require('server-statbot')({
   page_scoped_user_id: process.env.FB_USER_ID
 });
 
+// On every sshd log, message me
+let ssh = new Tail('/var/log/secure');
+ssh.on('line', data => console.log('sshd:', data));
+ssh.on('error', err => console.error('sshd tail error:', err));
+
 // On every log output from pm2, message me
 pm2.connect(() => {
   pm2.launchBus((err, bus) => {
-    bus.on('log:out', packet => { // apparently '\u000A' works
-      statbot.say('[' + packet.process.name + '] ' + packet.data.replace('\\n', '\n'));
+    bus.on('log:out', packet => { // apparently '\u000A' works instead of '\n'
+      statbot.say('[' + packet.process.name + '] ' + packet.data.replace('\n', '\u000A'));
     });
     bus.on('log:err', packet => {
-      statbot.say('[' + packet.process.name + '][err] ' + packet.data.replace('\\n', '\n'));
+      statbot.say('[' + packet.process.name + '][err] ' + packet.data.replace('\n', '\u000A'));
     });
   });
 });
